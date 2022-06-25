037d21bdd8e0db5a494e23e1ab3fb4270a2b70c4
konradlepek <konradlepek160@gmail.com>
2022-06-25 23:12:37 +0200

    MORE SECURITY

--- main.py
+++ main.py
@@ -72,7 +72,6 @@ def form(pojazd, dom, podroz, zycie, rolne, firma):
         string_submit = ",".join(list_submit_only_insurance)
         session["insurance"] = string_submit
 
-
         return redirect(url_for("full"))
     return render_template("index_2.html", pojazd=pojazd, dom=dom, podroz=podroz, zycie=zycie, rolne=rolne, firma=firma)
 
@@ -88,14 +87,16 @@ def full():
     # @@@@@@@@@@@@@@@ SENDING MAIL @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     msg = Message(f'{session["form_name"]} {session["form_lastname"]} - APK', sender='sender of the email',
                   recipients=[session.get("agent")])
-    msg.body = f"Wyniki apk: \n {session.get('insurance')} "
-    # msg.html = render_template("index_1.html")
+    # msg.body = f"Wyniki apk: \n {session.get('insurance') } \n Komentarz: {session['form_message']} "
+    msg.html = render_template("messages/message.html")
     mail.send(msg)
     return render_template("index_4_win.html")
+
 
 @app.route('/message')
 def message():
     return render_template("messages/message.html")
+
 
 @app.route('/ok')
 def ok():
